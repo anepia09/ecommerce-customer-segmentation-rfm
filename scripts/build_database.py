f5365aa74e6fdf58f9554174a1f5ff3de79a8012
Anevia Bakone <anevia93b@gmail.com>
2026-09-03 11:42:10 +0700

    Update build_database.py

--- scripts/build_database.py
+++ scripts/build_database.py
@@ -1,13 +1,10 @@
 import sqlite3
 import pandas as pd
 
-# Load cleaned data
 df = pd.read_csv('data/cleaned_data.csv')
 
-# SQLite database file
 conn = sqlite3.connect('data/retail.db')
 
-# Dataframe into a SQL table called 'transactions'
 df.to_sql('transactions', conn, if_exists='replace', index=False)
 
 print("Database created: data/retail.db")
